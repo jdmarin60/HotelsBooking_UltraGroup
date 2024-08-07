9646b5c009119516ffe3fce427f28b6f98e194e8
jdmarin60 <jdmarin60@gmail.com>
2024-08-07 05:23:23 -0500

    Adding DTO Pattern to Project

---
+++ HotelsBooking/HotelsBooking/Bookings/Application/DTOs/BookingCreateDTO.cs
@@ -0,0 +1,18 @@
+﻿using HotelsBooking.Guests.Domain;
+using HotelsBooking.Hotels.Domain;
+
+namespace HotelsBooking.Bookings.Application.DTOs
+{
+    public class BookingCreateDTO
+    {
+        public DateTime EntryDate { get; set; } = new DateTime();
+        public DateTime DepartureDate { get; set; } = new DateTime();
+        public string City { get; set; } = string.Empty;
+        public int PeopleAmount { get; set; }
+        public int HotelId { get; set; }
+        public Hotel Hotel { get; set; }
+        public List<Guest> Guests { get; set; } = new List<Guest>();
+        public DateTime CreatedAt { get; set; } = new DateTime();
+        public DateTime ModifyDate { get; set; } = new DateTime();
+    }
+}
---
+++ HotelsBooking/HotelsBooking/Bookings/Application/DTOs/BookingProfile.cs
@@ -0,0 +1,14 @@
+﻿using HotelsBooking.Bookings.Domain;
+using AutoMapper;
+
+namespace HotelsBooking.Bookings.Application.DTOs
+{
+    public class BookingProfile : Profile
+    {
+        public BookingProfile()
+        {
+            CreateMap<Booking, BookingReadDTO>();
+            CreateMap<BookingCreateDTO, Booking>();
+        }
+    }
+}
