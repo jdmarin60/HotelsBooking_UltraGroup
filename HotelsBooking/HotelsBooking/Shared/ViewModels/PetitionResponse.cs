d85c123f960b4252183519aea80981f9ab40bddd
jdmarin60 <jdmarin60@gmail.com>
2024-08-07 05:24:07 -0500

    Adding Helper Classes to Send Objects beetwen layers

---
+++ HotelsBooking/HotelsBooking/Shared/ViewModels/PetitionResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace HotelsBooking.Shared.ViewModels
+{
+    public class PetitionResponse
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public object Result { get; set; }
+    }
+}
